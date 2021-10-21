8b6c1cf9047c652b38dde4b523f03a769d0ab289
Leo Barros <leodbarros25@gmail.com>
2021-10-21 19:47:49 +0530

    Update application.py

--- application.py
+++ application.py
@@ -34,8 +34,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# Configure CS50 Library to use POSTGRES database
+db = SQL(os.getenv("DATABASE_URI"))
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
