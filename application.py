011a06e03317cd786b9cf4bbbeb233cbcad9ed01
Leo Barros <69587193+leo-barros@users.noreply.github.com>
2023-11-28 21:13:42 +0530

    Update application.py

--- application.py
+++ application.py
@@ -47,6 +47,7 @@ def index():
     current_price={}
     response={}
     total={}
+    session["user_id"]=0
     query= db.execute("SELECT userid,symbol,name,quantity FROM holdings WHERE userid=:userid ",userid=session["user_id"])
     cashq= db.execute("SELECT cash FROM users WHERE id=:userid",userid=session["user_id"])
     gtotal=cashq[0]["cash"]
